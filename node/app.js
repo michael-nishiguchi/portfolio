a6d60cd65af7f5b5db54b4c1b0b76328d20418b8
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-09-01 12:56:27 -0600

    add node portfolio

--- node/app.js
+++ node/app.js
@@ -39,3 +39,30 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+//EMAIL
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'mikenishiguchi@gmail.com',
+		pass: 'goiania1314'
+	}
+});
+
+var mailOptions = {
+	from: 'youremail@gmail.com',
+	to: 'myfriend@yahoo.com',
+	subject: 'Sending Email using Node.js',
+	text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+	if (error) {
+		console.log(error);
+	}
+	else {
+		console.log('Email sent: ' + info.response);
+	}
+});
