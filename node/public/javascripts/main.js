45a765402db3eb9d80f6c5acc2b0a796ae4b08b2
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2021-07-22 15:57:34 -0400

    redesign homepage

--- node/public/javascripts/main.js
+++ node/public/javascripts/main.js
@@ -13,44 +13,108 @@ function closeNav() {
 window.onload = function() {
 	var checkbox = document.querySelector('input[name=theme]');
 
+	var light = true;
+
 	checkbox.addEventListener('change', function() {
 		console.log('here');
 		if (this.checked) {
 			trans();
+			light = false;
 			document.documentElement.setAttribute('data-theme', 'dark');
 		} else {
 			trans();
+			light = true;
 			document.documentElement.setAttribute('data-theme', 'light');
 		}
 	});
 
-	document.getElementById('close-modal').addEventListener('click', function() {
-		document.getElementById('modal').style.display = 'none';
-	});
+	if (document.getElementById('close-modal') != null) {
+		document.getElementById('close-modal').addEventListener('click', function() {
+			document.getElementById('modal').style.display = 'none';
+		});
+	}
 
-	//display adobe projects
-	// document.getElementById('adobe a').addEventListener('click', function() {
-	document.querySelector('#adobe a').addEventListener('click', function() {
-		console.log('here');
-		document.getElementsByClassName('adobe-item').toggleClass('show');
-		document.getElementById('contact').toggleClass('transition-contact');
-	});
-	// 	$('.adobe-item').toggleClass('show');
-	// 	$('#contact').toggleClass('transition-contact');
-	// });
-
-	// $('#adobe a').click(function() {
-	// 	$('.adobe-item').toggleClass('show');
-	// 	$('#contact').toggleClass('transition-contact');
+	// document.querySelector('#adobe a').addEventListener('click', function() {
+	// 	console.log('here');
+	// 	document.getElementsByClassName('adobe-item').toggleClass('show');
+	// 	document.getElementById('contact').toggleClass('transition-contact');
 	// });
 
 	//check if window @ width
 	if (window.innerWidth > 1200 && document.getElementById('mySidenav').style.width == '350px') {
 		document.getElementById('mySidenav').style.width = '100%';
 	}
-	// } else {
-	// 	document.getElementById('mySidenav').style.width = 'auto';
-	// }
+
+	//canvas animation
+	var c = document.getElementById('canv');
+	var $ = c.getContext('2d');
+	var w = (c.width = 550);
+	var h = (c.height = 550);
+	var position = [w / 2, h / 2];
+	var spacing = 30;
+	var numw = w / spacing;
+	var numh = h / spacing;
+
+	var grayOpaque = 'rgba(67, 67, 67, .7)';
+	var gray = 'rgba(67, 67, 67, 1)';
+	var yellow = 'rgba(238, 155, 2, 1)';
+	var blue = 'rgba(2, 86, 238, .7)';
+	var lightBackground = 'rgba(226, 225, 224, 1)';
+	var lightBackgroundOpaque = 'rgba(226, 225, 224, .7)';
+
+	var background;
+	var backgroundOpaque;
+	var color;
+
+	var draw = function(p) {
+		if (light == true) {
+			background = lightBackground;
+			backgroundOpaque = lightBackgroundOpaque;
+			color = yellow;
+		} else {
+			background = grayOpaque;
+			backgroundOpaque = gray;
+			color = blue;
+		}
+
+		$.fillStyle = backgroundOpaque;
+		$.fillRect(0, 0, w, h);
+
+		$.fillStyle = background;
+
+		$.strokeStyle = color;
+		for (var i = 0; i < numh; i++)
+			for (var j = 0; j < numw; j++) {
+				var diagnalW = j * spacing + (i % 2 ? 0 : spacing / 2);
+				var diagnalH = i * spacing;
+				var arr = [position[0] - diagnalW, position[1] - diagnalH],
+					wave = Math.sqrt(arr[0] * arr[0] + arr[1] * arr[1]),
+					arr = [arr[0] / wave, arr[1] / wave],
+					angle = 50 * (Math.cos(p / 360 - wave / 105) - 1);
+				$.beginPath();
+				$.arc(diagnalW + arr[0] * angle, diagnalH + arr[1] * angle, 2.8, 0, 2 * Math.PI, false);
+				$.closePath();
+				$.fill();
+				for (var n = 0; n < 5; n++) {
+					var tail = 50 * (Math.cos((p - 50 * n) / 360 - wave / 105) - 1);
+					$.beginPath();
+					$.moveTo(diagnalW + arr[0] * angle, diagnalH + arr[1] * angle);
+					$.lineWidth = 5 - n;
+					$.lineTo(diagnalW + arr[0] * tail, diagnalH + arr[1] * tail);
+					$.stroke();
+				}
+			}
+	};
+	var anim = function(p) {
+		window.requestAnimationFrame(anim);
+		draw(p);
+	};
+	anim();
+
+	/****************47 Lines****
+Above Canvas Text Lines (#42-52) are unncessary && therefore do not count as part of the 50 Lines -
+Because I said so.
+*******************/
 };
 
 let trans = () => {
